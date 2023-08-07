2fd78450dfe4d9b7881edb31970d4da806a7eb80
Seth Rider <seth@ridersblock.net>
2023-08-07 21:39:14 +0100

    the dumbest repl

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,23 @@
 const std = @import("std");
 
+fn readline() !void {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    var buf: [10]u8 = undefined;
+
+    try stdout.print("🥚", .{});
+
+    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        std.debug.print("entered {s}\n", .{user_input});
+        // return user_input;
+    }
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    while (true) {
+        try readline();
+    }
 }
 
 test "simple test" {
